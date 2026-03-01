a664cd8f38d4a6ca9a361c26b3baf413d9f7370f
Emanuel Ferm <gemore@gmail.com>
2026-03-01 13:45:27 -0500

    nvim search all files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -428,6 +428,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+      vim.keymap.set('n', '<leader>sa', function() builtin.find_files { cwd = '~' } end, { desc = '[S]earch [A]ll files from home' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
