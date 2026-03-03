3bc54563899b4f1b1843ae33f49e8de875079133
Emanuel Ferm <gemore@gmail.com>
2026-03-03 15:30:16 -0500

    python dev env

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -610,7 +610,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        basedpyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
