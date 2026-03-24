0994883322a1e11319531afcdc6012f750224e20
Emanuel Ferm <gemore@gmail.com>
2026-03-24 14:19:03 -0400

    add neovim ide integration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -942,6 +942,15 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  {
+    'coder/claudecode.nvim',
+    opts = {
+      terminal = {
+        provider = 'none',
+      },
+    },
+  },
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
